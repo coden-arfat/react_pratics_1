98dde151509e058c0b42207bbd1726f3760b6ae7
arfat <coden.arfat@gmail.com>
2022-02-21 02:23:21 -0800

    v-4

--- src/index.js
+++ src/index.js
@@ -4,24 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 // import Car from './car'
-// import test_props_preant from './practics_props'
-function test_props_child(props){
-  return <p>hello {props.name}</p>;
-  
-}
-function test_props_preant(){
-  return(
-<>
-<h1>
-      hello i am from test_props_preant
-    </h1>
-    <test_props_child name='arfat'/>
-</>
-    
-  )
-}
+import Test_props_preant from './practics_props'
 ReactDOM.render(
- <test_props_preant /> ,
+,
   document.getElementById('root')
 );
 
--- src/practics_props.js
+++ src/practics_props.js
@@ -1,18 +1,29 @@
 import React from "react";
 
-function test_props_child(props){
-  return <p>hello {props.name}</p>;
-  
-}
-function test_props_preant(){
+function Card(props){
   return(
-<>
+    <div className="card">
+  <h2>{props.item}</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolores asperiores repudiandae nisi, sunt esse ipsa sit. Delectus dolores eius unde doloremque, in error provident blanditiis autem similique minus consequuntur.</p>
+</div>
+  )
+
+}
+function Test_props_child(props){
+   const list = props.name
+   const list_all = list.map((i)=>{
+     return <li><Card item={i}/></li>
+   })
+  return (<p>{list_all}</p>)
+  }
+function Test_props_preant(){
+  let persaon = ['arfat',17,'male']
+  return(
 <h1>
       hello i am from test_props_preant
-    </h1>
-    <test_props_child name='arfat'/>
-</>
-    
+      <Test_props_child name={persaon}/>
+</h1>  
   )
 }
-export default test_props_preant ;
+
+export default Test_props_preant ;
